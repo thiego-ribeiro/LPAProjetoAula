2a83e2ad35c50a4363b5c0d8f70fc74676c89466
Thiego Ribeiro <143758434+thiego-ribeiro@users.noreply.github.com>
2024-04-15 16:56:09 -0300

    Signed-off-by: Thiego Ribeiro <143758434+thiego-ribeiro@users.noreply.github.com>

---
+++ DiagramaClasseJogo/EntityFactory.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class EntityFactory:
+    def __init__(self):
+        self.entity_name = None
+
+    def get_entity(self, entity_type):
+        pass
+
---
+++ DiagramaClasseJogo/Player.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+from Entity import Entity
+
+class Player(Entity, Entity):
+    def __init__(self):
+        self.move = None
+
+    def move(self, ):
+        pass
+
